2ea9fccaae0acbf427b9fd8f82b89e293c0d8e48
rinshaj <rinshajdx4@gmail.com>
2021-05-11 18:37:25 +0530

    "moved up login handle to parent component"

--- src/components/LoginBox.js
+++ src/components/LoginBox.js
@@ -2,53 +2,20 @@ import "../css/Base.css";
 import "../css/UI-Components.css";
 import React, { useState } from "react";
 import useForm from "../hooks/useForm";
-import { useHistory } from "react-router-dom";
 
-export default function LoginBox() {
+export default function LoginBox(props) {
 	const [formInputs, changeFormInputs] = useForm({ username: ``, password: `` });
-	const [loginError, setLoginError] = useState(null);
-	const history = useHistory();
 
 	const handleSubmit = async (event) => {
 		event.preventDefault();
-		const username = formInputs.username;
-		const password = formInputs.password;
-
-		try {
-			const res = await fetch("http://127.0.0.1:5000/login", {
-				method: "POST",
-				headers: {
-					"Content-type": "application/json; charset=UTF-8",
-				},
-				body: JSON.stringify({ username: username, password: password }),
-			});
-			const data = await res.json();
-
-			if (!data.user) {
-				setLoginError("user not found");
-				return;
-			}
-
-			if (!data.user.type) {
-				setLoginError("incorrect password");
-				return;
-			}
-
-			setLoginError(null);
-			history.push({
-				pathname: "/home",
-				state: { user_id: data.user.type },
-			});
-		} catch (error) {
-			console.log(error);
-		}
+		props.onLogin(formInputs.username, formInputs.password);
 	};
 
 	return (
 		<div className={`login-box`}>
 			<h1>Login</h1>
 			<form onSubmit={handleSubmit}>
-				{loginError && <div className="field error">{loginError}</div>}
+				{props.loginError && <div className="field error">{props.loginError}</div>}
 				<div className={`field`}>
 					<label>username</label>
 					<input
--- src/routes/LoginPage.js
+++ src/routes/LoginPage.js
@@ -1,10 +1,47 @@
 import React from "react";
 import LoginBox from "../components/LoginBox";
+import { useState } from "react";
+import { useHistory } from "react-router";
 
-export default function LoginPage() {
+export default function LoginPage(props) {
+	const [loginError, setLoginError] = useState(null);
+	const history = useHistory();
+
+	const handleLogin = async (username, password) => {
+		try {
+			const res = await fetch("http://127.0.0.1:5000/login", {
+				method: "POST",
+				headers: {
+					"Content-type": "application/json; charset=UTF-8",
+				},
+				body: JSON.stringify({ username: username, password: password }),
+			});
+			const data = await res.json();
+
+			if (!data.user) {
+				setLoginError("user not found");
+				return;
+			}
+
+			if (!data.user.type) {
+				setLoginError("incorrect password");
+				return;
+			}
+
+			setLoginError(null);
+			history.push({
+				pathname: "/home",
+				state: { user_id: data.user.type },
+			});
+		} catch (error) {
+			setLoginError("Sorry, something went wrong");
+			console.log(error);
+		}
+	};
+
 	return (
 		<div className="page-container">
-			<LoginBox />
+			<LoginBox onLogin={handleLogin} loginError={loginError} />
 		</div>
 	);
 }
