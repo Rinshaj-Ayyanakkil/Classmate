f695d72cd15e25fd69ff0e44c4f0da0b7315efea
rinshaj <rinshajdx4@gmail.com>
2021-05-12 14:51:25 +0530

    developed register page

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { generateKey } from "../Globals";
+import useFetch from "../hooks/useFetch";
 import useForm from "../hooks/useForm";
 
 export default function RegisterForm() {
 	const [formInputs, changeFormInputs] = useForm({
 		id: ``,
-		username: "",
-		password: "",
+		username: ``,
+		password: ``,
+		confirmPassword: ``,
 	});
 
-	const handleSubmit = () => {};
+	const [response] = useFetch(`${process.env.REACT_APP_SERVER_URL}/ids`);
+	const [ids, setIds] = useState([]);
+
+	useEffect(() => (response ? setIds(response.ids) : setIds([])), [response]);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	};
 
 	return (
-		<div class="register-box">
+		<div className="register-form form-box">
 			<h1>Sign Up</h1>
 			<form onSubmit={handleSubmit}>
+				<div className="field">
+					<label>Select your register number</label>
+					<select
+						name="password"
+						type="password"
+						value={formInputs.password}
+						onChange={changeFormInputs}
+						required
+					>
+						{ids.map((id) => (
+							<option key={generateKey(id)}>{id}</option>
+						))}
+					</select>
+				</div>
 				<div className="field">
 					<label>username</label>
 					<input
@@ -35,7 +59,17 @@ export default function RegisterForm() {
 						required
 					/>
 				</div>
-				<div className={`field`}>
+				<div className="field">
+					<label>Confirm Password</label>
+					<input
+						name="confirmPassword"
+						type="password"
+						value={formInputs.confirmPassword}
+						onChange={changeFormInputs}
+						required
+					/>
+				</div>
+				<div className="field">
 					<button className="submit-button" type="submit">
 						Register
 					</button>
--- src/routes/RegisterPage.js
+++ src/routes/RegisterPage.js
@@ -1,9 +1,11 @@
 import React from "react";
+import RegisterForm from "../components/RegisterForm";
 
 export default function RegisterPage() {
 	return (
-		<div>
-			<RegisterPage />
+		<div className="page-container">
+			Register Page
+			<RegisterForm />
 		</div>
 	);
 }
