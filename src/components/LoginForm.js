8d824a63efc28ede258f1376f05b4090bbee4e01
rinshaj <rinshajdx4@gmail.com>
2021-05-13 23:06:53 +0530

    added form validation

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,9 +1,32 @@
 import "../css/Base.css";
 import "../css/UI-Components.css";
 import useForm from "../hooks/useForm";
+import { Link } from "react-router-dom";
 
 export default function LoginBox(props) {
-	const [formInputs, changeFormInputs] = useForm({ username: ``, password: `` });
+	const formFields = {
+		username: {
+			value: ``,
+			validations: [
+				{ pattern: /^.{1,}$/, message: `username cant be empty` },
+				{
+					pattern: /^.{1,250}$/,
+					message: `username must be less than 250 characters`,
+				},
+				{ pattern: /^\S*$/, message: `username should not contain spaces` },
+			],
+		},
+		password: {
+			value: ``,
+			validations: [
+				{
+					pattern: /^.{8,}$/,
+					message: `password must contain minimum 8 characters `,
+				},
+			],
+		},
+	};
+	const [formInputs, changeFormInputs, formErrors] = useForm(formFields);
 
 	const handleSubmit = async (event) => {
 		event.preventDefault();
@@ -12,7 +35,9 @@ export default function LoginBox(props) {
 
 	return (
 		<div className="form-box login-form">
-			<h1>Login</h1>
+			<div className="header">
+				<h1>Login</h1>
+			</div>
 			<form onSubmit={handleSubmit}>
 				{props.loginError && <span className="error">{props.loginError}</span>}
 				<div className={`field`}>
@@ -25,6 +50,7 @@ export default function LoginBox(props) {
 						autoComplete="off"
 						required
 					/>
+					{formErrors.username && <div className="error">{formErrors.username}</div>}
 				</div>
 				<div className={`field`}>
 					<label>password</label>
@@ -35,13 +61,28 @@ export default function LoginBox(props) {
 						onChange={changeFormInputs}
 						required
 					/>
+					{formErrors.password && <div className="error">{formErrors.password}</div>}
 				</div>
 				<div className={`field`}>
-					<button className="submit-button" type="submit">
+					<button
+						className="submit-button"
+						type="submit"
+						disabled={
+							Object.keys(formErrors).filter((field) => formErrors[field]).length !== 0
+						}
+					>
 						Log in
 					</button>
 				</div>
 			</form>
+			<div className="footer">
+				<div className="link">
+					<Link to="/register">Register</Link>
+				</div>
+				<div className="link">
+					<Link to="/reset-password">Forgot Password</Link>
+				</div>
+			</div>
 		</div>
 	);
 }
