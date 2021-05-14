ff408ebf9900d0c98628a37c35090539eee090d7
rinshaj <rinshajdx4@gmail.com>
2021-05-14 19:20:39 +0530

    Fix typo

--- src/components/LogoutBox.js
+++ src/components/LogoutBox.js
@@ -5,7 +5,7 @@ import React from "react";
 export default function LogoutBox({ onLogout, onCancel }) {
 	return (
 		<div className="logout-box-container">
-			<div className="header">Logout</div>
+			<div className="header">Log Out</div>
 			<div className="content">Are you sure?</div>
 			<div className="footer">
 				<button className="logout-button" onClick={onLogout}>
