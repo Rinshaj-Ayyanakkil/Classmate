a43dd39eb1a7143dddc7337fd3a91e2b8aac5171
rinshaj <rinshajdx4@gmail.com>
2021-05-17 14:17:08 +0530

    Strengthen key generation function

--- src/Globals.js
+++ src/Globals.js
@@ -3,7 +3,15 @@ export const fetchStudentImage = (rollno) => {
 };
 
 export const generateKey = (salt) => {
-	return `${salt}_${new Date().getTime()}`;
+	return (
+		salt +
+		([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+			(
+				c ^
+				(crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+			).toString(16)
+		)
+	);
 };
 
 export const getRandomNumber = (min, max) => {
