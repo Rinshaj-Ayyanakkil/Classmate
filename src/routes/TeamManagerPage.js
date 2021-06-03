a4144bdd5aa4fc63124282be224b2f0fa88306c9
rinshaj <rinshajdx4@gmail.com>
2021-06-04 03:09:35 +0530

    Add toggle switch manula/auto team generation

--- src/routes/TeamManagerPage.js
+++ src/routes/TeamManagerPage.js
@@ -5,6 +5,7 @@ import TeamSaveMenu from "../components/TeamSaveMenu";
 import TeamViewer from "../components/TeamViewer";
 import TeamCreator from "../components/TeamCreator";
 import { useStudents } from "../contexts/StudentsContext";
+import SliderSwitch from "../components/SliderSwitch";
 
 const TeamsContext = React.createContext();
 
@@ -13,6 +14,7 @@ export const useTeams = () => {
 };
 
 export default function TeamManagerPage() {
+	const [isAutoGeneration, setAutoGeneration] = useState(false);
 	const [teams, setTeams] = useState({});
 
 	const studentsData = useStudents();
@@ -28,12 +30,31 @@ export default function TeamManagerPage() {
 		);
 	}, [studentsData]);
 
+	const toggleAutoGeneration = () => {
+		setAutoGeneration(!isAutoGeneration);
+	};
+
 	return (
 		<div className="page-container">
 			<h1>Team Manager</h1>
+
+			<SliderSwitch
+				isChecked={isAutoGeneration}
+				onChange={toggleAutoGeneration}
+				dataOn="Auto"
+				dataOff="Manual"
+			/>
+
 			<TeamsContext.Provider value={[teams, setTeams]}>
-				{items.length !== 0 && <TeamCreator itemList={items} />}
-				{items.length !== 0 && <TeamGenerator itemList={items} />}
+				{items.length !== 0 && (
+					<div>
+						{isAutoGeneration ? (
+							<TeamGenerator itemList={items} />
+						) : (
+							<TeamCreator itemList={items} />
+						)}
+					</div>
+				)}
 				<TeamViewer teams={teams} />
 				<TeamSaveMenu />
 				<GroupViewer />
