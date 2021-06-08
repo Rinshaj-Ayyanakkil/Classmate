82c8ac1c8558ff6425e279ac3c59a9db47c9be71
rinshaj <rinshajdx4@gmail.com>
2021-06-08 22:20:50 +0530

    Add models for groups

--- server/src/models.py
+++ server/src/models.py
@@ -28,3 +28,28 @@ class LoginModel(db.Model):
     username = db.Column(db.String(255), nullable=False)
     password = db.Column(db.String(255), nullable=False)
     is_admin = db.Column(db.Boolean, nullable=False, default=0)
+
+
+class GroupModel(db.Model):
+    __tablename__ = "groups_info"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    teams = db.relationship("TeamModel", back_ref="groups_info", lazy="dynamic")
+
+
+class TeamModel(db.Model):
+    __tablename__ = "teams_info"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    group_id = db.Column(db.Integer, db.ForeignKey("groups_info.id"), nullable=False)
+    team_members = db.relationship("TeamMemberModel", back_ref="teams_info", lazy="dynamic")
+
+
+class TeamMemberModel(db.Model):
+    __tablename__ = "team_members"
+
+    id = db.Column(db.Integer, primary_key=True)
+    roll_number = db.Column(db.Integer, db.ForeignKey("students_details.roll_number"), nullable=False)
+    team_id = db.Column(db.Integer, db.ForeignKey("teams_info.id"), nullable=False)
