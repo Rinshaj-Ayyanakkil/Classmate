cd1ac6582e4daa92264b11fabe83a64c536086e7
rinshaj <rinshajdx4@gmail.com>
2021-06-09 01:11:23 +0530

    Replace useFetch hook with doFetch function for fetching saved groups

--- client/src/components/GroupViewer.js
+++ client/src/components/GroupViewer.js
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import { generateKey } from "../Globals";
-import useFetch from "../hooks/useFetch";
 import TeamViewer from "./TeamViewer";
 
 export default function GroupViewer({ teams }) {
-	const [res] = useFetch(`${process.env.REACT_APP_SERVER_URL}/teams`);
+	const [groups, setGroups] = useState([]);
+
+	const fetchSavedGroups = useCallback(async () => {
+		try {
+			const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/groups`, {
+				method: "GET",
+				headers: {
+					"Content-type": "application/json; charset=UTF-8",
+				},
+			});
+			if (response.ok) {
+				const data = await response.json();
+				setGroups(data?.groups);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}, []);
+
+	useEffect(() => fetchSavedGroups);
 
 	return (
 		<div className="group-view-container">
-			{res?.groups &&
-				res.groups.map((group) => (
-					<div className="group-view" key={generateKey()}>
-						<h1>{group.title}</h1>
-						<TeamViewer teams={group.teams} />
-					</div>
-				))}
+			<h1>Saved Groups</h1>
+			{groups.map((group) => (
+				<div className="group-view" key={generateKey(group.id)}>
+					<h1>{group.title}</h1>
+					<TeamViewer teams={group.teams} />
+				</div>
+			))}
 		</div>
 	);
 }
