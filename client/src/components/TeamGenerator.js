4a68d0cff26b8bb6b06e53583324dc68a3d1bf47
rinshaj <rinshajdx4@gmail.com>
2021-06-10 02:54:28 +0530

    Remove member count input and optimize team generation

--- client/src/components/TeamGenerator.js
+++ client/src/components/TeamGenerator.js
@@ -1,10 +1,10 @@
 import "../css/Base.css";
 import "../css/UI-Components.css";
-import React, { useEffect, useState, useCallback, useContext } from "react";
-import { getRandomNumber, shuffleArray } from "../Globals";
+import React, { useState, useContext } from "react";
+import { shuffleArray } from "../Globals";
 import { useTeams } from "../routes/TeamManagerPage";
 import CollapsableFieldset from "./CollapsibleFieldset";
-import SliderSwitch from "./SliderSwitch";
+import useForm from "../hooks/useForm";
 
 const ItemsContext = React.createContext();
 
@@ -13,96 +13,72 @@ export const useItems = () => {
 };
 
 export default function TeamGenerator({ itemList }) {
-	const [userInput, setUserInput] = useState({ isTeamCount: true, value: 1 });
-	const handleInputChange = (event) => {
-		const [min, max, value] = [
-			Number(event.target.min),
-			Number(event.target.max),
-			event.target.value,
-		];
-
-		if (value !== "" && value < min) return;
-
-		if (value > max) return;
-
-		setUserInput({ ...userInput, value: value });
+	const formFields = {
+		teamCount: {
+			value: 1,
+			validations: [],
+		},
 	};
+	const [formInputs, changeFormInputs] = useForm(formFields);
 
 	const [items, setItems] = useState(() =>
 		itemList.map((item) => {
-			return { ...item, isParticipating: true };
+			return { ...item, isParticipating: true, assignedTeam: null };
 		})
 	);
+	const [, setTeams] = useTeams();
+
+	const teamModel = (id = 1, members = []) => {
+		return {
+			id: id,
+			title: `Team ${id}`,
+			members: members,
+		};
+	};
 
 	// team generator function
-	const generateTeams = useCallback((isTeamCount, value, items) => {
-		const teamCount = isTeamCount ? value : Math.ceil(items.length / value);
-		const memberCount = isTeamCount ? Math.floor(items.length / value) : value;
+	const generateTeams = (teamCount, items) => {
+		const memberCount = Math.ceil(items.length / teamCount);
+		if (teamCount === 0) return;
 
-		if (teamCount === Infinity || memberCount === Infinity) return {};
+		items = shuffleArray(items);
 
-		let teams = [];
-
-		for (let i = 0; i < teamCount; i++) {
-			const members = [];
-			for (let j = 0; j < memberCount; j++) {
-				if (items.length === 0) break;
-				shuffleArray(items);
-				const randomIndex = getRandomNumber(0, items.length - 1);
-				const randomItem = items[randomIndex];
-				members.push(randomItem);
-				items = items.filter((item) => item !== randomItem);
-			}
-			if (members.length === 0) break;
-			const newTeam = { id: i + 1, title: `Team ${i + 1}`, members: members };
+		const teams = [];
+		for (let i = 0, n = 0; i < teamCount; i++, n += memberCount) {
+			const members = items.slice(n, n + memberCount);
+			console.log(i, n, members);
+			const newTeam = teamModel(i + 1, members);
 			teams.push(newTeam);
 		}
 
-		//assigning leftover items evenly to existing teams
-		teams.every((team) => {
-			if (items.length === 0) return false;
-			team.members.push(items.pop());
-			return true;
-		});
-
 		return teams;
-	}, []);
+	};
 
-	const [, setTeams] = useTeams();
-
-	useEffect(
-		() =>
-			setTeams(
-				generateTeams(userInput.isTeamCount, userInput.value, [
-					...items.filter((item) => item.isParticipating),
-				])
-			),
-		[userInput, items, setTeams, generateTeams]
-	);
+	const handleTeamGeneration = (event) => {
+		event.preventDefault();
+		const teams = generateTeams(
+			formInputs.teamCount,
+			items.filter((item) => item.isParticipating)
+		);
+		setTeams(teams);
+	};
 
 	return (
 		<div className="team-generator-container">
-			<div className="toggle-input">
-				<SliderSwitch
-					isChecked={userInput.isTeamCount}
-					onChange={() =>
-						setUserInput({ ...userInput, isTeamCount: !userInput.isTeamCount })
-					}
-					dataOn="Team Count"
-					dataOff="Maximum Member Count"
-				/>
+			<form className="toggle-input" onSubmit={handleTeamGeneration}>
 				<label>
 					<input
 						type="number"
 						name="teamCount"
-						value={userInput.value}
-						onChange={handleInputChange}
+						value={formInputs.teamCount}
+						onChange={changeFormInputs}
 						min="1"
 						max={items.filter((item) => item.isParticipating).length}
 						required
 					/>
 				</label>
-			</div>
+				<button type="submit">generate</button>
+			</form>
 			<ItemsContext.Provider value={[items, setItems]}>
 				<CollapsableFieldset itemList={items} />
 			</ItemsContext.Provider>
