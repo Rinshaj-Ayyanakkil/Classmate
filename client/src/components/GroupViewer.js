5e6a8739a78fa6d7f0afaa815c613ef8f1fbbea2
rinshaj <rinshajdx4@gmail.com>
2021-06-10 17:13:31 +0530

    Fix infinite re-render

--- client/src/components/GroupViewer.js
+++ client/src/components/GroupViewer.js
@@ -4,25 +4,27 @@ import TeamViewer from "./TeamViewer";
 
 export default function GroupViewer({ teams }) {
 	const [groups, setGroups] = useState([]);
+	console.log(`fetched`);
 
-	const fetchSavedGroups = useCallback(async () => {
-		try {
-			const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/groups`, {
-				method: "GET",
-				headers: {
-					"Content-type": "application/json; charset=UTF-8",
-				},
-			});
-			if (response.ok) {
-				const data = await response.json();
-				setGroups(data?.groups);
+	useEffect(() => {
+		const fetchSavedGroups = async () => {
+			try {
+				const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/groups`, {
+					method: "GET",
+					headers: {
+						"Content-type": "application/json; charset=UTF-8",
+					},
+				});
+				if (response.ok) {
+					const data = await response.json();
+					setGroups(data?.groups);
+				}
+			} catch (error) {
+				console.log(error);
 			}
-		} catch (error) {
-			console.log(error);
-		}
-	}, []);
-
-	useEffect(() => fetchSavedGroups);
+		};
+		fetchSavedGroups();
+	}, [groups]);
 
 	return (
 		<div className="group-view-container">
