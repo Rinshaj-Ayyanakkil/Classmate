e635625e9722b3e6292a3837f7627d62b65356c2
rinshaj <rinshajdx4@gmail.com>
2021-06-12 01:42:09 +0530

    Use useReducer to manage team state instead of useState

--- client/src/routes/TeamManagerPage.js
+++ client/src/routes/TeamManagerPage.js
@@ -1,8 +1,7 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useContext, useEffect, useReducer } from "react";
 import GroupViewer from "../components/GroupViewer";
 import TeamGenerator from "../components/TeamGenerator";
 import TeamSaveMenu from "../components/TeamSaveMenu";
-import TeamViewer from "../components/TeamViewer";
 import { useStudents } from "../contexts/StudentsContext";
 
 const TeamsContext = React.createContext();
@@ -11,8 +10,37 @@ export const useTeams = () => {
 	return useContext(TeamsContext);
 };
 
+export const ACTIONS = {
+	UPDATE_TEAMS: "updateTeams",
+	SET_TEAMS: "resetTeams",
+	SET_MEMBERS_BY_ITEMS: "setMembersByItem",
+};
+
 export default function TeamManagerPage() {
-	const [teams, setTeams] = useState([]);
+	const reducer = (state, action) => {
+		switch (action.type) {
+			case ACTIONS.UPDATE_TEAMS:
+				return { ...state, teams: [...state.teams, ...action.payload.teams] };
+			case ACTIONS.SET_TEAMS:
+				return { ...state, teams: action.payload.teams };
+			case ACTIONS.SET_MEMBERS_BY_ITEMS:
+				return {
+					...state,
+					teams: state.teams.map((team) => {
+						return {
+							...team,
+							members: action.payload.items.filter(
+								(item) => item.assignedTeam === team.id
+							),
+						};
+					}),
+				};
+			default:
+				return state;
+		}
+	};
+
+	const [state, dispatch] = useReducer(reducer, { teams: [] });
 
 	const studentsData = useStudents();
 	const [items, setItems] = useState([]);
@@ -33,9 +61,8 @@ export default function TeamManagerPage() {
 		<div className="page-container">
 			<h1>Team Manager</h1>
 
-			<TeamsContext.Provider value={[teams, setTeams]}>
+			<TeamsContext.Provider value={[state.teams, dispatch]}>
 				{items.length !== 0 && <TeamGenerator itemList={items} />}
-				<TeamViewer teams={teams} />
 				<TeamSaveMenu />
 				<GroupViewer />
 			</TeamsContext.Provider>
