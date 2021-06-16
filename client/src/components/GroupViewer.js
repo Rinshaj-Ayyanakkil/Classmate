754a09061631f03db1ca23a95e582b744f29400e
rinshaj <rinshajdx4@gmail.com>
2021-06-17 01:36:00 +0530

    Fix focus changing on component mount

--- client/src/components/GroupViewer.js
+++ client/src/components/GroupViewer.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
+import useForm from "../hooks/useForm";
 import TeamViewer from "./TeamViewer";
 
 export default function GroupViewer({ group, onChangeTitle }) {
@@ -7,11 +8,30 @@ export default function GroupViewer({ group, onChangeTitle }) {
 	const titleRef = useRef(null);
 
 	useEffect(() => {
-		titleRef.current.focus();
+		if (isEditable) {
+			titleRef.current.focus();
+		}
 	}, [isEditable]);
 
-	const handleChangeTitle = (id, newTitle) => {
-		const isTitleChanged = onChangeTitle(group.id, newTitle);
+	const formFields = {
+		title: {
+			name: `title`,
+			initialValue: group?.title,
+			validations: [
+				{
+					pattern: (title) => {
+						return !(!title || /^\s*$/.test(title));
+					},
+					message: `Title cannot be empty!`,
+				},
+			],
+		},
+	};
+
+	const [formInputs, changeFormInputs, formErrors] = useForm(formFields);
+	const handleChangeTitle = (event) => {
+		event.preventDefault();
+		const isTitleChanged = onChangeTitle(group.id, formInputs.title);
 		// if title is changed successfully, editable is set false and vice versa
 		setEditable(!isTitleChanged);
 	};
@@ -19,17 +39,24 @@ export default function GroupViewer({ group, onChangeTitle }) {
 	return (
 		<div className="group-view">
 			<div className="header">
-				<h1 ref={titleRef} contentEditable={isEditable}>
-					{group.title}
-				</h1>
+				<form onSubmit={handleChangeTitle}>
+					<input
+						type="text"
+						name={formFields.title.name}
+						value={formInputs.title}
+						ref={titleRef}
+						onChange={changeFormInputs}
+						readOnly={!isEditable}
+						required
+					/>
+					{formErrors.title && <span>{formErrors.title}</span>}
+				</form>
 				{!isEditable ? (
-					<button onClick={() => setEditable(true)}>edit</button>
-				) : (
-					<button
-						onClick={() => handleChangeTitle(group.id, titleRef.current.innerText)}
-					>
-						save
+					<button type="button" onClick={() => setEditable(true)}>
+						edit
 					</button>
+				) : (
+					<button type="submit">save</button>
 				)}
 			</div>
 			<div className="content">
