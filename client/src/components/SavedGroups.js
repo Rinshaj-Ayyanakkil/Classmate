f1647138b7b8f4c6d18f9b765bfd096fa62ad235
rinshaj <rinshajdx4@gmail.com>
2021-06-17 01:37:00 +0530

    Replace api call with useFetch hook

--- client/src/components/SavedGroups.js
+++ client/src/components/SavedGroups.js
@@ -1,54 +1,49 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import GroupViewer from "./GroupViewer";
 import { generateKey } from "../Globals";
+import useFetch from "../hooks/useFetch";
 
 export default function SavedGroups() {
 	const [groups, setGroups] = useState([]);
 
 	useEffect(() => {
-		const fetchSavedGroups = async () => {
-			try {
-				const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/groups`, {
-					method: "GET",
-					headers: {
-						"Content-type": "application/json; charset=UTF-8",
-					},
-				});
-				if (response.ok) {
-					const data = await response.json();
-					setGroups(data?.groups);
-				}
-			} catch (error) {
-				console.log(error);
-			}
-		};
-		fetchSavedGroups();
-	}, []);
+		console.log(`Saved Groups rendered`);
+	});
+
+	const request = useMemo(
+		() => ({
+			url: `${process.env.REACT_APP_SERVER_URL}/groups`,
+			options: {
+				method: "GET",
+				headers: {
+					"Content-type": "application/json; charset=UTF-8",
+				},
+			},
+		}),
+		[]
+	);
+
+	const [response, isLoading, fetchData] = useFetch(request, true);
+
+	useEffect(() => {
+		if (response?.groups) setGroups(response.groups);
+	}, [response]);
+
+	const handleRefresh = () => {
+		console.log(`refresh`);
+		fetchData();
+	};
 
 	const changeGroupTitle = (id, newTitle) => {
-		let titleError = null;
-		newTitle = newTitle.trim();
 		// checking if the new title already exists (case insensitive)
-		if (
-			groups
-				.map((group) => group.id !== id && group.title.toLowerCase())
-				.includes(newTitle.toLowerCase)
-		) {
-			titleError = `${newTitle} already exists!`;
-		}
-		// checking if new title above 250 characters
-		if (newTitle.length > 250) {
-			titleError = `Title too long!`;
-		}
-		// checking if new title is blank
-		if (!newTitle || /^\s*$/.test(newTitle)) {
-			titleError = `Title cannot be empty!`;
-		}
-		// handling error in title
-		if (titleError) {
-			window.alert(titleError);
-			return false;
-		}
+		// if (
+		// 	groups
+		// 		.map((group) => group.id !== id && group.title.toLowerCase())
+		// 		.includes(newTitle.toLowerCase)
+		// ) {
+		// 	titleError = `${newTitle} already exists!`;
+		// }
+
 		// persisting the title change
 		setGroups(
 			groups.map((group) => {
@@ -60,13 +55,23 @@ export default function SavedGroups() {
 
 	return (
 		<div className="groups-container">
-			{groups.map((group) => (
-				<GroupViewer
-					group={group}
-					onChangeTitle={changeGroupTitle}
-					key={generateKey(group.id)}
-				/>
-			))}
+			<div className="header">
+				<h2>Saved Groups</h2>
+				<button onClick={handleRefresh}>refresh</button>
+			</div>
+			{isLoading
+				? "loading..."
+				: groups.map((group) => (
+						<GroupViewer
+							group={group}
+							onChangeTitle={changeGroupTitle}
+							key={generateKey(group.id)}
+						/>
+						// <div key={generateKey()}>
+						// 	<h1>{group.title}</h1>
+						// 	<ul>{group.teams.members}</ul>
+						// </div>
+				  ))}
 		</div>
 	);
 }
