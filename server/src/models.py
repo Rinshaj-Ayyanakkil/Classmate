607aa6f4451c7e3e83d7b95c76a3c7f7c18e27d3
rinshaj <rinshajdx4@gmail.com>
2021-06-19 03:01:41 +0530

    Fix typo

--- server/src/models.py
+++ server/src/models.py
@@ -21,8 +21,8 @@ class StudentModel(db.Model):
     class_id = db.Column(db.Integer, db.ForeignKey("classes.id"), nullable=False)
     roll_number = db.Column(db.Integer, nullable=False, unique=True)
     name = db.Column(db.String(255), nullable=False)
-    details = db.relationship("StudentsDetailsModel", backref="students", lazy="dynamic", uselist=False)
-    user = db.relationship("LoginModel", backref="students", lazy="dynamic", uselist=False)
+    details = db.relationship("StudentDetailsModel", backref="students", uselist=False)
+    user = db.relationship("LoginModel", backref="students", uselist=False)
 
 
 class StudentDetailsModel(db.Model):
