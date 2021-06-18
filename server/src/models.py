92d35152911e188a2c06e2eeeef1ed57f145871f
rinshaj <rinshajdx4@gmail.com>
2021-06-19 01:37:36 +0530

    Add class model

--- server/src/models.py
+++ server/src/models.py
@@ -1,15 +1,36 @@
+from enum import unique
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import backref
 
 db = SQLAlchemy()
 
 
+class ClassModel(db.Model):
+    __tablename__ = "classes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    students = db.relationship("StudentModel", backref="classes", lazy="dynamic")
+
+
 class StudentModel(db.Model):
+    __tablename__ = "students"
+
+    id = db.Column(db.Integer, primary_key=True)
+    class_id = db.Column(db.Integer, db.ForeignKey("classes.id"), nullable=False)
+    roll_number = db.Column(db.Integer, nullable=False, unique=True)
+    name = db.Column(db.String(255), nullable=False)
+    details = db.relationship("StudentsDetailsModel", backref="students", lazy="dynamic", uselist=False)
+    user = db.relationship("LoginModel", backref="students", lazy="dynamic", uselist=False)
+
+
+class StudentDetailsModel(db.Model):
     __tablename__ = "students_details"
 
-    roll_number = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, db.ForeignKey("students.id"), nullable=False)
     register_number = db.Column(db.String(255))
-    name = db.Column(db.String(255))
     date_of_birth = db.Column(db.Date)
     phone_number = db.Column(db.String(255))
     address = db.Column(db.String(255))
@@ -23,33 +44,34 @@ class StudentModel(db.Model):
 class LoginModel(db.Model):
     __tablename__ = "login"
 
-    user_id = db.Column(db.Integer, primary_key=True)
-    roll_number = db.Column(db.Integer, db.ForeignKey("students_details.roll_number"), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, db.ForeignKey("students.id"), nullable=False)
     username = db.Column(db.String(255), nullable=False)
     password = db.Column(db.String(255), nullable=False)
-    is_admin = db.Column(db.Boolean, nullable=False, default=0)
+    is_admin = db.Column(db.Boolean, nullable=False, server_default="false", default=False)
 
 
 class GroupModel(db.Model):
-    __tablename__ = "groups_info"
+    __tablename__ = "groups"
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
-    teams = db.relationship("TeamModel", backref="groups_info", lazy="dynamic")
+    teams = db.relationship("TeamModel", backref="groups", lazy="dynamic")
 
 
 class TeamModel(db.Model):
-    __tablename__ = "teams_info"
+    __tablename__ = "teams"
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
-    group_id = db.Column(db.Integer, db.ForeignKey("groups_info.id"), nullable=False)
-    team_members = db.relationship("TeamMemberModel", backref="teams_info", lazy="dynamic")
+    group_id = db.Column(db.Integer, db.ForeignKey("groups.id"), nullable=False)
+    team_members = db.relationship("TeamMemberModel", backref="teams", lazy="dynamic")
 
 
 class TeamMemberModel(db.Model):
     __tablename__ = "team_members"
 
     id = db.Column(db.Integer, primary_key=True)
-    roll_number = db.Column(db.Integer, db.ForeignKey("students_details.roll_number"), nullable=False)
-    team_id = db.Column(db.Integer, db.ForeignKey("teams_info.id"), nullable=False)
+    student_id = db.Column(db.Integer, db.ForeignKey("students.id"), nullable=False)
+    team_id = db.Column(db.Integer, db.ForeignKey("teams.id"), nullable=False)
+    is_leader = db.Column(db.Boolean, nullable=False, server_default="false", default=False)
