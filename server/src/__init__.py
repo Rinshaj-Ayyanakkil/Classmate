ef805a0420c700afe31f01461250083b0c5eadbb
rinshaj <rinshajdx4@gmail.com>
2021-07-06 03:29:39 +0530

    Add config files

--- server/src/__init__.py
+++ server/src/__init__.py
@@ -1,19 +1,20 @@
+import os
 from flask import Flask
 from .extensions import db, cors, bcrypt, migrate, api_obj
 from .api.main import api_bp
 
 
-def create_app():
+def create_app(config="config.py"):
 
     app = Flask(__name__)
+
+    app.config.from_pyfile(config)
+
     db.init_app(app)
     cors.init_app(app, resources={r"/*": {"origins": "*"}})
     bcrypt.init_app(app)
     migrate.init_app(app, db)
     api_obj.init_app(app)
-
-    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://rinshaj:@localhost/classmate"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     app.register_blueprint(api_bp)
 
