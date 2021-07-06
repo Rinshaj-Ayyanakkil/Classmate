44849930a93e0fec29b61641676c27adce676db3
rinshaj <rinshajdx4@gmail.com>
2021-07-06 20:51:07 +0530

    Change restful api initialiaztion with api blueprint instead of app

--- server/__init__.py
+++ server/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from server.extensions import db, cors, bcrypt, migrate, api_obj
-from server.api.api_bp import api_bp
+from server.api import api_bp
 
 
 def create_app(config="config.py"):
@@ -10,11 +10,12 @@ def create_app(config="config.py"):
     app.config.from_pyfile(config)
 
     db.init_app(app)
+
     cors.init_app(app, resources={r"/*": {"origins": "*"}})
     bcrypt.init_app(app)
     migrate.init_app(app, db)
-    api_obj.init_app(app)
 
-    app.register_blueprint(api_bp)
+    api_obj.init_app(api_bp)
+    app.register_blueprint(api_bp, url_prefix="/api")
 
     return app
